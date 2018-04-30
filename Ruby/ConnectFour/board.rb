d390ab19e4214eeecf7658e3b27ab2985b04572b
DxExWxExY <DxExWxExY@gmail.com>
2018-04-29 22:11:24 -0600

    20 Points Earned

--- Ruby/ConnectFour/board.rb
+++ Ruby/ConnectFour/board.rb
@@ -4,81 +4,98 @@
 # borrowing liberally from Gregory Brown's tic-tac-toe game
 
 class Board
-  def initialize
-    @board = [[nil, nil, nil, nil, nil, nil, nil],
-              [nil, nil, nil, nil, nil, nil, nil],
-              [nil, nil, nil, nil, nil, nil, nil],
-              [nil, nil, nil, nil, nil, nil, nil],
-              [nil, nil, nil, nil, nil, nil, nil],
-              [nil, nil, nil, nil, nil, nil, nil]]
-  end
-
-  def add_discs(first_player, columns)
-    players = if first_player == :R
-                %i[R O].cycle
-              else
-                %i[O R].cycle
-              end
-    columns.each { |c| add_disc(players.next, c) }
-  end
-
-  def add_disc(player, column)
-    if column >= 7 || column < 0
-      puts "  add_disc(#{player},#{column}): out of bounds"
-      return false
+    def initialize
+        @board = [[nil, nil, nil, nil, nil, nil, nil],
+                  [nil, nil, nil, nil, nil, nil, nil],
+                  [nil, nil, nil, nil, nil, nil, nil],
+                  [nil, nil, nil, nil, nil, nil, nil],
+                  [nil, nil, nil, nil, nil, nil, nil],
+                  [nil, nil, nil, nil, nil, nil, nil]]
     end
 
-
-    first_free_row = @board.transpose.slice(column).rindex(nil)
-
-    if first_free_row.nil?
-      puts "  add_disc(#{player},#{column}): column full already"
-      return false
+    def add_discs(first_player, columns)
+        players = if first_player == :R
+                      %i[R O].cycle
+                  else
+                      %i[O R].cycle
+                  end
+        columns.each {|c| add_disc(players.next, c)}
     end
-    update(first_free_row, column, player)
-    true
-  end
 
-  def update(row, col, player)
-    @board[row][col] = player
-  end
+    def add_disc(player, column)
+        if column >= 7 || column < 0
+            puts "  add_disc(#{player},#{column}): out of bounds"
+            return false
+        end
 
-  def get_element(row, col)
-    @board[row][col]
-  end
 
-  def get_column(col)
-    @board.transpose.slice(col)
-  end
+        first_free_row = @board.transpose.slice(column).rindex(nil)
 
-  def print
-    puts @board.map { |row| row.map { |e| e || ' ' }.join('|') }.join("\n")
-    puts "1 2 3 4 5 6 7\n"
-  end
+        if first_free_row.nil?
+            puts "  add_disc(#{player},#{column}): column full already"
+            return false
+        end
+        update(first_free_row, column, player)
+        true
+    end
 
-  def has_won?(player)
-    vertical_win?(player) | horizontal_win?(player) |
-        diagonal_up_win?(player) | diagonal_down_win?(player)
-  end
+    def pop_disc(column)
+        discs = @board.transpose.slice(column).take(5)
+        i = 0
+        while i < 6
+            @board[i][column] = nil
+            i+=1
+        end
+        discs.reverse.each do |e|
+            add_disc(e, column)
+        end
+    end
 
-  def vertical_win?(player)
-    (0..6).any? { |c| (0..2).any? { |r| four_from_towards?(player, r, c, 1, 0) } }
-  end
+    def update(row, col, player)
+        @board[row][col] = player
+    end
 
-  def horizontal_win?(player)
-    (0..3).any? { |c| (0..5).any? { |r| four_from_towards?(player, r, c, 0, 1) } }
-  end
+    def get_element(row, col)
+        @board[row][col]
+    end
 
-  def diagonal_up_win?(player)
-    (0..3).any? { |c| (0..2).any? { |r| four_from_towards?(player, r, c, 1, 1) } }
-  end
+    def get_column(col)
+        @board.transpose.slice(col)
+    end
 
-  def diagonal_down_win?(player)
-    (0..3).any? { |c| (3..5).any? { |r| four_from_towards?(player, r, c, -1, 1) } }
-  end
+    def print
+        puts @board.map {|row| row.map {|e| e == nil ? "( )" : "(#{e})"}.join(' ')}.join("\n")
+        puts ' 1 | 2 | 3 | 4 | 5 | 6 | 7 '
+    end
 
-  def four_from_towards?(player, r, c, dx, dy)
-    (0..3).all? { |step| @board[r + step * dx][c + step * dy] == player }
-  end
+    def has_won?(player)
+        vertical_win?(player) | horizontal_win?(player) |
+            diagonal_up_win?(player) | diagonal_down_win?(player)
+    end
+
+    def is_full?(column)
+        col = @board.transpose.slice(column).rindex(nil)
+        col.nil?
+    end
+
+    def vertical_win?(player)
+        (0..6).any? {|c| (0..2).any? {|r| four_from_towards?(player, r, c, 1, 0)}}
+    end
+
+    def horizontal_win?(player)
+        (0..3).any? {|c| (0..5).any? {|r| four_from_towards?(player, r, c, 0, 1)}}
+    end
+
+    def diagonal_up_win?(player)
+        (0..3).any? {|c| (0..2).any? {|r| four_from_towards?(player, r, c, 1, 1)}}
+    end
+
+    def diagonal_down_win?(player)
+        (0..3).any? {|c| (3..5).any? {|r| four_from_towards?(player, r, c, -1, 1)}}
+    end
+
+    def four_from_towards?(player, r, c, dx, dy)
+        (0..3).all? {|step| @board[r + step * dx][c + step * dy] == player}
+    end
 
 end
--- Ruby/ConnectFour/robot.rb
+++ Ruby/ConnectFour/robot.rb
@@ -2,124 +2,176 @@ load 'board.rb'
 
 class Robot
 
-  @player_move = nil
-  @last_move = nil
-  @board = nil
-  @block_flag = false
+    @player_move = nil
+    @last_move = nil
+    @board = nil
+    @block_flag = false
 
-  def initialize(game)
-    @board = game
-  end
-
-  def block(move)
-    @player_move = move
-    if block_vertical?
-      @board.add_disc(:R, @player_move)
-      @block_flag = false
-      puts "Blocked Vertically"
-    elsif block_horizontal_right?
-      @board.add_disc(:R, @player_move)
-      @block_flag = false
-      puts "Blocked Horizontally R"
-      @last_move = @player_move
-    elsif block_horizontal_left?
-      @board.add_disc(:R, @player_move)
-      @block_flag = false
-      puts "Blocked Horizontally L"
-      @last_move = @player_move
-    elsif !@last_move.nil?
-      @board.add_disc(:R, @last_move)
-      @block_flag = false
-    elsif @last_move.nil?
-      @board.add_disc(:R, @player_move)
-      @last_move = @player_move
-      @block_flag = false
+    def initialize(game)
+        @board = game
     end
-  end
 
-  def block_diagonal?
-
-  end
-
-  def block_horizontal_right?
-    original_pos = @player_move
-    indices = @board.get_column(@player_move)
-    indices = indices.each_index.select {|e| indices[e] == :O}
-    indices.each do |index|
-      s = @player_move + 1
-      e = @player_move + 3
-      while s <= e && s < 7 && s >= 0
-        if s != e && @board.get_element(index, s) == :O
-          @block_flag = true
-          if @player_move + 1 < 7 && original_pos - 1 >= 0
-            @player_move =  original_pos - 1
-          else
-            @player_move = s + 1
-          end
-        elsif s == e && @board.get_element(index, s) == :O
-          @block_flag |= true
-          if @player_move + 1 < 7 && original_pos - 1 >= 0
-            @player_move =  original_pos - 1
-          else
-            @player_move = s + 1
-          end
-        else
-          return @block_flag
+    def block(move)
+        @player_move = move
+        if block_vertical?
+            insert(@player_move)
+            @last_move = @player_move
+            puts "V"
+        elsif block_diagonal_right?
+            insert(@player_move)
+            @last_move = @player_move
+            puts "DR"
+        elsif block_horizontal_left?
+            insert(@player_move)
+            @last_move = @player_move
+            puts "DL"
+        elsif block_horizontal_right?
+            insert(@player_move)
+            @last_move = @player_move
+            puts "HR"
+        elsif block_horizontal_left?
+            insert(@player_move)
+            @last_move = @player_move
+            puts "HL"
+        elsif !@last_move.nil?
+            insert(@last_move)
+            puts "DEF1"
+        elsif @last_move.nil?
+            insert(@player_move)
+            @last_move = @player_move
+            puts "DEF2"
         end
-        s += 1
-      end
     end
-    @block_flag
-  end
 
-  def block_horizontal_left?
-    original_pos = @player_move
-    indices = @board.get_column(@player_move)
-    indices = indices.each_index.select {|e| indices[e] == :O}
-    indices.each do |index|
-      s = @player_move - 1
-      e = @player_move - 2
-      while s >= e && s < 7 && s >= 0
-        puts "S: " + s.to_s
-        if s != e && @board.get_element(index, s) == :O
-          @block_flag = true
-          if @player_move - 1 >= 0 && original_pos + 1 < 7
-            @player_move =  original_pos + 1
-          else
-            @player_move = s - 1
-          end
-        elsif s == e && @board.get_element(index, s) == :O
-          @block_flag |= true
-          if @player_move - 1 >= 0 && original_pos + 1 < 7
-            @player_move =  original_pos + 1
-          else
-            @player_move = s - 1
-          end
+    def insert(position)
+        if @board.is_full?(position)
+            if @last_move != position && @board.is_full?(@last_move)
+                @board.add_disc(:R, @last_move)
+            else
+                @last_move = (1 - @player_move) % 7
+                @board.add_disc(:R, @last_move)
+            end
         else
-          return @block_flag
+            @board.add_disc(:R, position)
         end
-        s -= 1
-      end
+        @block_flag = false
     end
-    @block_flag
-  end
 
-  def block_vertical?
-    indices = @board.get_column(@player_move)
-    indices = indices.each_index.select {|e| indices[e] == :O}
-    indices.each do |index|
-      s = index - 1
-      e = index - 2
-      while s <= e && s < 6 && s >= 0
-        if s != e
-          @block_flag &= @board.get_element(@player_move, s) == :O
-        else
-          @block_flag |= @board.get_element(@player_move, s) == :O
-        end
-        s -= 1
-      end
+    def in_range(row, col)
+        row < 6 && col < 7 && row >= 0 && col >= 0
     end
-    @block_flag
-  end
+
+    def block_diagonal_right?
+        indices = @board.get_column(@player_move)
+        indices = indices.each_index.select {|e| indices[e] == :O}
+        indices.each do |index|
+            row = index + 1
+            col = @player_move + 1
+            if in_range(row + 1, col + 1)
+                if @board.get_element(row, col) == :O && @board.get_element(row + 1, col + 1) == :O
+                    return true
+                else
+                    return false
+                end
+            end
+            return false
+        end
+    end
+
+    def block_diagonal_left?
+        indices = @board.get_column(@player_move)
+        indices = indices.each_index.select {|e| indices[e] == :O}
+        indices.each do |index|
+            row = index + 1
+            col = @player_move - 1
+            if in_range(row + 1, col - 1)
+                if @board.get_element(row, col) == :O && @board.get_element(row + 1, col - 1) == :O
+                    return true
+                else
+                    return false
+                end
+            end
+            return false
+        end
+    end
+
+    def block_horizontal_right?
+        original_pos = @player_move
+        indices = @board.get_column(@player_move)
+        indices = indices.each_index.select {|e| indices[e] == :O}
+        indices.each do |index|
+            s = @player_move + 1
+            e = @player_move + 3
+            while s <= e && s < 7 && s >= 0
+                if s != e && @board.get_element(index, s) == :O
+                    @block_flag = true
+                    if @player_move + 1 < 7 && original_pos - 1 >= 0
+                        @player_move = original_pos - 1
+                    else
+                        @player_move = s + 1
+                    end
+                elsif s == e && @board.get_element(index, s) == :O
+                    @block_flag |= true
+                    if @player_move + 1 < 7 && original_pos - 1 >= 0
+                        @player_move = original_pos - 1
+                    else
+                        @player_move = s + 1
+                    end
+                else
+                    return @block_flag
+                end
+                s += 1
+            end
+        end
+        @block_flag
+    end
+
+    def block_horizontal_left?
+        original_pos = @player_move
+        indices = @board.get_column(@player_move)
+        indices = indices.each_index.select {|e| indices[e] == :O}
+        indices.each do |index|
+            s = @player_move - 1
+            e = @player_move - 2
+            while s >= e && s < 7 && s >= 0
+                if s != e && @board.get_element(index, s) == :O
+                    @block_flag = true
+                    if @player_move - 1 >= 0 && original_pos + 1 < 7
+                        @player_move = original_pos + 1
+                    else
+                        @player_move = s - 1
+                    end
+                elsif s == e && @board.get_element(index, s) == :O
+                    @block_flag |= true
+                    if @player_move - 1 >= 0 && original_pos + 1 < 7
+                        @player_move = original_pos + 1
+                    else
+                        @player_move = s - 1
+                    end
+                else
+                    return @block_flag
+                end
+                s -= 1
+            end
+        end
+        @block_flag
+    end
+
+    def block_vertical?
+        indices = @board.get_column(@player_move)
+        indices = indices.each_index.select {|e| indices[e] == :O}
+        indices.each do |index|
+            s = index + 1
+            e = index + 2
+            if s <= e && e < 6 && s < 6
+                if @board.get_element(s, @player_move) == :O && @board.get_element(e, @player_move) == :O
+                    return true
+                else
+                    return false
+                end
+            end
+        end
+        @block_flag
+    end
 
 end
